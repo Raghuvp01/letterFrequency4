4753d372d9382b933f3d60433a60a4895765ba00
Raghunandan <nandan.raghu002@gmail.com>
2021-08-01 01:41:55 +0530

    Task: updated routes.py

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,17 @@ app = Flask(__name__, instance_relative_config=False)
 @app.route('/')
 def dashboard():
     return render_template(
-        'dashboard.jinja2',
-        title="Login Page",
-        description="Smarter page templates with Flask & Jinja."
+        "dashboard.jinja2",
+        title="Flask-Session Tutorial.",
+        template="dashboard-template",
+        body="You are now logged in!",
+    )
+
+
+@app.route('/')
+def session_view():
+    return render_template(
+        "session.jinja2",
+        title="Flask-Session Tutorial.",
+        template="dashboard-template",
     )
